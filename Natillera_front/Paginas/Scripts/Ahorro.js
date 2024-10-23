b8ea3cc44b0f20fd659a341a57d0ca740216251a
JULIAN ESTEBAN SUAREZ GALLEGO <juliansuarez281405@correo.itm.edu.co>
2024-10-23 16:21:38 -0500

    se agregaron los combos

--- Natillera_front/Paginas/Scripts/Ahorro.js
+++ Natillera_front/Paginas/Scripts/Ahorro.js
@@ -11,17 +11,47 @@ class Ahorro {
 // Función para llenar la tabla de ahorros
 jQuery(function () {
     LlenarTabla(); // Llenar la tabla de ahorros
+    LlenarComboClientes();
 });
 
 function LlenarTabla() {
     LlenarTablaXServicios("http://localhost:50745/api/Ahorros/LlenarTablaAhorros", "#tblAhorros");
 }
 
+function LlenarComboClientes() {
+    const url = "http://localhost:50745/api/Clientes/LlenarCombo";
+    LlenarComboCliente(url, "#cmbCliente");
+}
+async function LlenarComboCliente(URLServicio, ComboLlenar) {
+    try {
+        const Respuesta = await fetch(URLServicio, {
+            method: "GET",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        const Rpta = await Respuesta.json();
+
+        // Limpiar el combo antes de llenarlo
+        $(ComboLlenar).empty();
+        $(ComboLlenar).append('<option value="">Seleccione un cliente</option>');
+
+        // Llenar el combo con los datos recibidos
+        for (let i = 0; i < Rpta.length; i++) {
+            $(ComboLlenar).append('<option value="' + Rpta[i].clienteID + '">' + Rpta[i].nombre + '</option>');
+        }
+    } catch (error) {
+        $("#dvMensaje").html("Error al cargar el combo: " + error);
+    }
+}
+
 // Función genérica para ejecutar comandos de inserción, actualización y eliminación para Ahorro
 async function EjecutarComando(Metodo, Funcion) {
     const ahorro = new Ahorro(
         $("#txtAhorroID").val(),
-        $("#txtClienteID").val(),
+        $("#cmbCliente").val(),
         $("#txtMontoMensual").val(),
         $("#txtFechaInicial").val()
     );
@@ -49,12 +79,12 @@ async function Consultar() {
     let URL = "http://localhost:50745/api/Ahorros/ConsultarXID?id=" + ahorroID;
     const ahorro = await ConsultarServicio(URL);
     if (ahorro != null) {
-        $("#txtClienteID").val(ahorro.clienteID);
+        $("#cmbCliente").val(ahorro.clienteID);
         $("#txtMontoMensual").val(ahorro.montoMensual);
         $("#txtFechaInicial").val(ahorro.fechaInicial);
     } else {
         $("#dvMensaje").html("El ahorro no está en la base de datos");
-        $("#txtClienteID").val("");
+        $("#cmbCliente").val("");
         $("#txtMontoMensual").val("");
         $("#txtFechaInicial").val("");
     }
--- Natillera_front/Paginas/Scripts/Deposito.js
+++ Natillera_front/Paginas/Scripts/Deposito.js
@@ -1,5 +1,6 @@
 ﻿jQuery(function () {
     LlenarTabla(); // Llenar la tabla de depósitos
+    LlenarComboClientes(); // Llenar el combo de clientes
 });
 
 // Función para llenar la tabla de depósitos
@@ -7,13 +8,41 @@ function LlenarTabla() {
     LlenarTablaXServicios("http://localhost:50745/api/Depositos/LlenarTablaDepositos", "#tblDepositos");
 }
 
+function LlenarComboClientes() {
+    const url = "http://localhost:50745/api/Clientes/LlenarCombo";
+    LlenarComboCliente(url, "#cmbCliente");
+}
+async function LlenarComboCliente(URLServicio, ComboLlenar) {
+    try {
+        const Respuesta = await fetch(URLServicio, {
+            method: "GET",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        const Rpta = await Respuesta.json();
+
+        // Limpiar el combo antes de llenarlo
+        $(ComboLlenar).empty();
+        $(ComboLlenar).append('<option value="">Seleccione un cliente</option>');
+
+        // Llenar el combo con los datos recibidos
+        for (let i = 0; i < Rpta.length; i++) {
+            $(ComboLlenar).append('<option value="' + Rpta[i].clienteID + '">' + Rpta[i].nombre + '</option>');
+        }
+    } catch (error) {
+        $("#dvMensaje").html("Error al cargar el combo: " + error);
+    }
+}
 // Función genérica para ejecutar comandos de inserción, actualización y eliminación
 async function EjecutarComando(Metodo, Funcion) {
     const deposito = new Deposito(
         $("#txtDepositoID").val(),
         $("#txtMonto").val(),
         $("#txtFecha").val(),
-        $("#txtCliente").val(), 
+        $("#cmbCliente").val(), 
         $("#txtAhorro").val()   
     );
     let URL = "http://localhost:50745/api/Depositos/" + Funcion;
@@ -38,8 +67,8 @@ class Deposito {
         this.depositoID = DepositoID;
         this.monto = Monto;
         this.fecha = Fecha;
-        this.cliente = Cliente;
-        this.ahorro = Ahorro;
+        this.clienteID = Cliente;
+        this.ahorroID = Ahorro;
     }
 }
 
@@ -51,13 +80,13 @@ async function Consultar() {
     if (deposito != null) {
         $("#txtMonto").val(deposito.monto);
         $("#txtFecha").val(deposito.fecha);
-        $("#txtCliente").val(deposito.cliente);
+        $("#cmbCliente").val(deposito.cliente);
         $("#txtAhorro").val(deposito.ahorro);
     } else {
         $("#dvMensaje").html("El depósito no está en la base de datos");
         $("#txtMonto").val("");
         $("#txtFecha").val("");
-        $("#txtCliente").val("");
+        $("#cmbCliente").val("");
         $("#txtAhorro").val("");
     }
 }
